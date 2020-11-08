8167906a3782454a5480551d72289bb58f9be305
Коптяев Александр <a.koptyaev@playrix.com>
2020-11-09 00:48:06 +0300

    Модуль GUI

---
+++ Source/GuiManager.cpp
@@ -0,0 +1,9 @@
+#include "GuiManager.h"
+
+#include <iostream>
+
+using namespace gui;
+
+void GuiManager::init() {
+	std::cout << "GuiManager init";
+};
---
+++ Source/GuiManager.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+namespace gui {
+	class GuiManager
+	{
+	public:
+		static void GuiManager::init();
+	};
+}
